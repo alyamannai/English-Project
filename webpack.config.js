1fd1109f340fb14130eeb5f4862170f63f454401
alyamannai <alyamannaimat@gmail.com>
2023-09-30 20:42:54 +0400

    finished header started in tripOverview

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, "dist")
+    },
+    module:{
+        rules:[
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+              }, 
+        ],
+        
+    },
+plugins: [
+        new HtmlWebpackPlugin({
+            template: 'dist/index.html', // Template path in the 'dist' directory
+        }),
+        new HtmlWebpackPlugin({
+            template: 'dist/contact.html', // Template path in the 'dist' directory
+        }),
+        new HtmlWebpackPlugin({
+            template: 'dist/price.html', // Template path in the 'dist' directory
+        }),
+        new HtmlWebpackPlugin({
+            template: 'dist/location.html', // Template path in the 'dist' directory
+        }),
+    ]
+};
+
